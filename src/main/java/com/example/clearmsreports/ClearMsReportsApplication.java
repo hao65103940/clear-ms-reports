6dafabea2139369e148e4d49b36c3c2ef8599dde
fit2cloudzhao <452988409@qq.com>
2024-02-08 13:02:02 +0800

    first commit

---
+++ src/main/java/com/example/clearmsreports/ClearMsReportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clearmsreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearMsReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearMsReportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clearmsreports/ClearMsReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearmsreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearMsReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
